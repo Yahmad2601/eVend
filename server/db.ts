7dd44f7516fbb88e2fa53eb3e7925751fef8199b
Ahmad Yahaya <yahmad2601@gmail.com>
2025-09-12 18:56:04 +0100

    Last6

--- server/db.ts
+++ server/db.ts
@@ -1,6 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import * as schema from "../shared/schema";
+import * as schema from "../shared/schema.js";
 
 // This checks if the DATABASE_URL is provided in your environment variables.
 if (!process.env.DATABASE_URL) {
